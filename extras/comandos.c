c60df76810f7842f8fdc37a1649bf69f1342ac2e
AlexLopezDevelop <alex_lopez.es@icloud.com>
2022-01-05 23:28:49 +0100

    added: check is parameter is number

--- extras/comandos.c
+++ extras/comandos.c
@@ -10,6 +10,8 @@
 
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
+
 
 int comandos_establecer_conexion() {
 
@@ -203,124 +205,136 @@ int comandos_propios(char **instruccion, int totalParams, int socketFD, Usuario 
 
     if (strcmp("LOGIN", comando) == 0) {
         if (totalParams == 2) {
-            socketFD = comandos_establecer_conexion();
-            //char *data = funciones_concat_strings_por_asterico(instruccion[1], instruccion[2]);
-            char *data;
-            asprintf(&data, "%s*%s", instruccion[1], instruccion[2]);
-            char *trama = comandos_obtener_trama('C', data);
-            write(socketFD, trama, MAX_TRAMA_SIZE);
+            if (isdigit(*instruccion[2])) {
+                socketFD = comandos_establecer_conexion();
+                //char *data = funciones_concat_strings_por_asterico(instruccion[1], instruccion[2]);
+                char *data;
+                asprintf(&data, "%s*%s", instruccion[1], instruccion[2]);
+                char *trama = comandos_obtener_trama('C', data);
+                write(socketFD, trama, MAX_TRAMA_SIZE);
 
 
-            char tramaRespuesta[MAX_TRAMA_SIZE];
-            read(socketFD, tramaRespuesta, MAX_TRAMA_SIZE);
+                char tramaRespuesta[MAX_TRAMA_SIZE];
+                read(socketFD, tramaRespuesta, MAX_TRAMA_SIZE);
 
-            if (tramaRespuesta[15] == 'O') {
-                //idUser
-                usuario->id = comandos_extraer_id_trama(tramaRespuesta);
-                //nombreUser
-                usuario->nombre = strdup(instruccion[1]);
-                //CodigoPostalUser
-                usuario->codigoPostal = strdup(instruccion[2]);
-                sprintf(print, "Benvingut %s. Tens ID %d.\nAra estàs connectat a Atreides.\n\n", usuario->nombre,
-                        usuario->id);
-                funciones_display(print);
-                usuario->socketFD = socketFD;
+                if (tramaRespuesta[15] == 'O') {
+                    //idUser
+                    usuario->id = comandos_extraer_id_trama(tramaRespuesta);
+                    //nombreUser
+                    usuario->nombre = strdup(instruccion[1]);
+                    //CodigoPostalUser
+                    usuario->codigoPostal = strdup(instruccion[2]);
+                    sprintf(print, "Benvingut %s. Tens ID %d.\nAra estàs connectat a Atreides.\n\n", usuario->nombre,
+                            usuario->id);
+                    funciones_display(print);
+                    usuario->socketFD = socketFD;
+                }
+
+            } else {
+                funciones_display("El segundo parametro ha de ser un numero\n");
             }
-
         } else {
             funciones_display("Comanda KO. Falta paràmetres\n");
         }
     } else if (strcmp("PHOTO", comando) == 0) {
         if (totalParams == 1) {
-            char *data;
-            asprintf(&data, "%s", instruccion[1]);
-            char *trama = comandos_obtener_trama('P', data);
-            write(usuario->socketFD, trama, MAX_TRAMA_SIZE);
-
-            char tramaRespuesta[MAX_TRAMA_SIZE];
-            read(usuario->socketFD, tramaRespuesta, MAX_TRAMA_SIZE);
-
-            ConexionData *conexionData = ficheros_guardar_trama(tramaRespuesta);
-            FotoData *fotoData = ficheros_destruct_data_imagen(conexionData->datos);
-
-            bool descargandoImagen = true;
-            char tramaImagen[MAX_TRAMA_SIZE];
-
-            fotoData->totalTramas = fotoData->size / TRAMA_DATA_SIZE;
-            if (fotoData->size % TRAMA_DATA_SIZE != 0) {
-                fotoData->totalTramas++;
-            }
-
-            while (descargandoImagen) {
-                memset(tramaImagen, 0, TRAMA_DATA_SIZE);
-                memset(conexionData, 0, sizeof (ConexionData));
-                read(usuario->socketFD, tramaImagen, MAX_TRAMA_SIZE);
-
-                int fd;
-
-                fd = open(fotoData->nombre, O_WRONLY | O_CREAT | O_APPEND, 00666);
-
-                if (funciones_error_abrir(fd)) {
-                    funciones_display("Error al guardar la imagen\n");
-                }
-
-                conexionData = ficheros_guardar_trama(tramaImagen);
-
-                if (fotoData->size % TRAMA_DATA_SIZE != 0 && (fotoData->totalTramas - 1) == i) {
-                    write(fd, conexionData->datos, sizeof(char) * (fotoData->size % TRAMA_DATA_SIZE));
-                    i = 0;
-                    descargandoImagen = false;
-                } else {
-                    write(fd, conexionData->datos, sizeof(char) * TRAMA_DATA_SIZE);
-                    i++;
-                }
-
-                close(fd);
-            }
-
-            funciones_display("Foto descargada :)\n");
-
-        } else {
-            funciones_display("Comanda KO. Massa paràmetres\n");
-        }
-    } else if ((strcmp("SEARCH", comando) == 0)) {
-        if (totalParams == 1) {
-            if (usuario->socketFD > 0) {
-                char idString[20];
-                sprintf(idString, "%d", usuario->id);
+            if (isdigit(*instruccion[1])) {
                 char *data;
-                asprintf(&data, "%s*%s*%s", usuario->nombre, idString, instruccion[1]);
-                char *trama = comandos_obtener_trama('S', data);
-
+                asprintf(&data, "%s", instruccion[1]);
+                char *trama = comandos_obtener_trama('P', data);
                 write(usuario->socketFD, trama, MAX_TRAMA_SIZE);
 
                 char tramaRespuesta[MAX_TRAMA_SIZE];
                 read(usuario->socketFD, tramaRespuesta, MAX_TRAMA_SIZE);
 
-                if (tramaRespuesta[15] == 'L') {
-                    char auxid[30];
+                ConexionData *conexionData = ficheros_guardar_trama(tramaRespuesta);
+                FotoData *fotoData = ficheros_destruct_data_imagen(conexionData->datos);
 
-                    ListadoUsuarios *listadoUsuarios = comandos_destruct_trama_respuesta(tramaRespuesta);
+                bool descargandoImagen = true;
+                char tramaImagen[MAX_TRAMA_SIZE];
 
-                    funciones_display("\n");
-                    sprintf(print, "Hi han %d persones humanes a %s\n", listadoUsuarios->total, instruccion[1]);
-                    funciones_display(print);
-                    funciones_display("\n");
+                fotoData->totalTramas = fotoData->size / TRAMA_DATA_SIZE;
+                if (fotoData->size % TRAMA_DATA_SIZE != 0) {
+                    fotoData->totalTramas++;
+                }
 
-                    for (int j = 0; j < listadoUsuarios->total; ++j) {
-                        sprintf(auxid, "%d", listadoUsuarios->usuarios[j].id);
-                        sprintf(print, "%s ", auxid);
-                        funciones_display(print);
-                        funciones_display(listadoUsuarios->usuarios[j].nombre);
-                        funciones_display("\n");
+                while (descargandoImagen) {
+                    memset(tramaImagen, 0, TRAMA_DATA_SIZE);
+                    memset(conexionData, 0, sizeof(ConexionData));
+                    read(usuario->socketFD, tramaImagen, MAX_TRAMA_SIZE);
+
+                    int fd;
+
+                    fd = open(fotoData->nombre, O_WRONLY | O_CREAT | O_APPEND, 00666);
+
+                    if (funciones_error_abrir(fd)) {
+                        funciones_display("Error al guardar la imagen\n");
                     }
 
-                    funciones_display("\n");
-                } else if (tramaRespuesta[15] == 'K') {
-                    funciones_display("No hay ningun usuario con este codigo postal\n");
+                    conexionData = ficheros_guardar_trama(tramaImagen);
+
+                    if (fotoData->size % TRAMA_DATA_SIZE != 0 && (fotoData->totalTramas - 1) == i) {
+                        write(fd, conexionData->datos, sizeof(char) * (fotoData->size % TRAMA_DATA_SIZE));
+                        i = 0;
+                        descargandoImagen = false;
+                    } else {
+                        write(fd, conexionData->datos, sizeof(char) * TRAMA_DATA_SIZE);
+                        i++;
+                    }
+
+                    close(fd);
                 }
+
+                funciones_display("Foto descargada :)\n");
             } else {
-                funciones_display("Has de realizar el login primero\n");
+                funciones_display("El segundo parametro ha de ser un numero\n");
+            }
+        } else {
+            funciones_display("Comanda KO. Massa paràmetres\n");
+        }
+    } else if ((strcmp("SEARCH", comando) == 0)) {
+        if (totalParams == 1) {
+            if (isdigit(*instruccion[1])) {
+                if (usuario->socketFD > 0) {
+                    char idString[20];
+                    sprintf(idString, "%d", usuario->id);
+                    char *data;
+                    asprintf(&data, "%s*%s*%s", usuario->nombre, idString, instruccion[1]);
+                    char *trama = comandos_obtener_trama('S', data);
+
+                    write(usuario->socketFD, trama, MAX_TRAMA_SIZE);
+
+                    char tramaRespuesta[MAX_TRAMA_SIZE];
+                    read(usuario->socketFD, tramaRespuesta, MAX_TRAMA_SIZE);
+
+                    if (tramaRespuesta[15] == 'L') {
+                        char auxid[30];
+
+                        ListadoUsuarios *listadoUsuarios = comandos_destruct_trama_respuesta(tramaRespuesta);
+
+                        funciones_display("\n");
+                        sprintf(print, "Hi han %d persones humanes a %s\n", listadoUsuarios->total, instruccion[1]);
+                        funciones_display(print);
+                        funciones_display("\n");
+
+                        for (int j = 0; j < listadoUsuarios->total; ++j) {
+                            sprintf(auxid, "%d", listadoUsuarios->usuarios[j].id);
+                            sprintf(print, "%s ", auxid);
+                            funciones_display(print);
+                            funciones_display(listadoUsuarios->usuarios[j].nombre);
+                            funciones_display("\n");
+                        }
+
+                        funciones_display("\n");
+                    } else if (tramaRespuesta[15] == 'K') {
+                        funciones_display("No hay ningun usuario con este codigo postal\n");
+                    }
+                } else {
+                    funciones_display("Has de realizar el login primero\n");
+                }
+
+            } else {
+                funciones_display("El segundo parametro ha de ser un numero\n");
             }
         } else {
             funciones_display("Comanda KO. Massa paràmetres\n");
@@ -329,7 +343,7 @@ int comandos_propios(char **instruccion, int totalParams, int socketFD, Usuario 
         if (totalParams == 1) {
             char sizeFileString[100];
 
-            int fd = open(instruccion[1],O_RDONLY);
+            int fd = open(instruccion[1], O_RDONLY);
             if (fd > 0) {
                 close(fd);
                 int sizeFile = funciones_get_file_size(instruccion[1]);
@@ -355,8 +369,6 @@ int comandos_propios(char **instruccion, int totalParams, int socketFD, Usuario 
             } else {
                 funciones_display("Error. No existe la imagen\n\n");
             }
-
-
 
 
         } else {
